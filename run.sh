f17f73c58c12d3b6e96bda2b8383f85c20f94240
Krishna Nallamilli <krishna@kimo.ai>
2021-04-04 12:00:59 +0530

    Seperated Dockerfile dev and prod

--- run.sh
+++ run.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-proj_name=udemy
+proj_name=<Enter Project Name>
+
+dev_container="${proj_name}_dev"
+prod_container="${proj_name}_prod"
 
 dev_build="${proj_name}:dev"
+prod_build="${proj_name}:prod"
 	
 	
 case $1 in
 	"build")
-		docker build -t ${dev_build} .
+		docker build -t ${dev_build} -f Dockerfile_dev .
 		;;
 	"run")
-		docker run --rm -ti --name ${proj_name} -v $(pwd):/home/jovyan/work -p 8888:8888 ${dev_build}
+		docker run --rm -ti --name ${dev_container} -v $(pwd):/home/jovyan/work -p 8888:8888 ${dev_build}
 		;;
+	"build-prod")
+		docker build -t ${prod_build} -f Dockerfile .
+		;;
+	"run-prod")
+		#docker run --rm -ti --name ${prod_container} -v $(pwd)/app:/app -p 8081:80 ${prod_build}
+		docker run --rm -ti --name ${prod_container} -p 8081:80 ${prod_build}
+		;;	
 	*)
-		echo "Wrong Command"
+		echo "dev_container -> ${dev_container}"
 		;;
 esac
